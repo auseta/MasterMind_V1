88ba54a63a011b8f44a5f112a3c62e32b6c6812b
augusto <augusto@192.168.0.11>
2023-06-29 00:15:10 -0300

    Se creó el proyecto MasterMind. Se creó la clase principal del proyecto. Se creó y configuró el archivo pom.xml.

---
+++ src/main/java/main/MasterMind1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package main;
+
+/**
+ *
+ * @author augusto
+ */
+public class MasterMind1 {
+
+    public static void main(String[] args) {
+        
+    }
+}
